d21a7f461b64a15fb6da321442f8f9183f01ddae
Ebuube <onwutaebubegideon1555@gmail.com>
2023-10-07 21:07:02 +0000

    Task 0: Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,24 +4,28 @@
 # Install Nginx
 # apt-get -y purge nginx nginx-common
 # apt-get -y autoremove
-apt-get -y update
-apt-get -y install nginx
+apt-get --assume-yes update
+apt-get --assume-yes install nginx
 ufw allow 'Nginx HTTP'
 
 # Folders
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
+mkdir --parents /data/
+
+# Ownership
+chown --recursive "$USER":"$USER" /data/
+
+mkdir --parents /data/web_static/
+mkdir --parents /data/web_static/releases/
+mkdir --parents /data/web_static/shared/
+mkdir --parents /data/web_static/releases/test/
 
 # Fake HTML file
 echo '<html>
-	<head>
-	</head>
-	<body>
-		Holberton School
-	</body>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
 </html>' > /data/web_static/releases/test/index.html
 
 
@@ -29,21 +33,16 @@ echo '<html>
 web_current=/data/web_static/current
 test_dir=/data/web_static/releases/test/
 
-if [ -L "$web_current" ]; then
-	# rm --recursive "$web_current";
-	ln --symbolic --force "$test_dir" "$web_current"
-else
-	ln --symbolic --force "$test_dir" "$web_current"
+if [ -e "$web_current" ]; then
+	rm --force "$web_current"
 fi
-
-# Ownership
-chown -R "$USER":"$USER" /data/
+ln --symbolic "$test_dir" "$web_current"
 
 # Update Nginx configuration
 nginx_conf=/etc/nginx/sites-enabled/default
 
 # shellcheck disable=SC2016
-printf %s '# Configuration for server - ONWUTA EBUBE GIDEON
+echo '# Configuration for server - ONWUTA EBUBE GIDEON
 server {
 	listen 80 default_server;
 	listen [::]:80 default_server;
@@ -65,18 +64,6 @@ server {
 		# First attempt to serve a request as file, then
 		# as directiory, then fall back to dispalying a 404 ERROR.
 		try_files $uri $uri/ =404;
-	}
-
-	location /redirect_me {
-		# Have fun with redirection
-		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-	}
-
-	# Create a custom 404 error page
-	error_page 404 /404.html;
-	location = /404.html {
-		# to ensure it cannot be accessed directly by clients
-		internal;
 	}
 }' > "$nginx_conf"
 
