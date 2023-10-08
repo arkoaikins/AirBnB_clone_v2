592d3dc2e59c07813d50c11a369042494d5fb341
Ebuube <onwutaebubegideon1555@gmail.com>
2023-10-08 10:10:41 +0000

    Task 0: Prepare your web servers - updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,6 +4,7 @@
 # Install Nginx
 # apt-get -y purge nginx nginx-common
 # apt-get -y autoremove
+
 apt-get --assume-yes update
 apt-get --assume-yes install nginx
 ufw allow 'Nginx HTTP'
@@ -37,9 +38,21 @@ if [ -e "$web_current" ]; then
 	rm --force "$web_current"
 fi
 ln --symbolic "$test_dir" "$web_current"
+chown --recursive ubuntu:ubuntu "$web_current"
+
 
 # Update Nginx configuration
 nginx_conf=/etc/nginx/sites-enabled/default
+
+default_index='/var/www/html/index.html'
+SITE='/var/www/html'
+error_page="$SITE/404.html"
+
+mkdir --parents "$SITE"
+chown --recursive ubuntu:ubuntu "$SITE"
+
+printf "Hello World!\n" > "$default_index"
+printf "Ceci n'est pas une page\n" > "$error_page"
 
 # shellcheck disable=SC2016
 echo '# Configuration for server - ONWUTA EBUBE GIDEON
@@ -49,10 +62,19 @@ server {
 
 	root /var/www/html;
 
+	# Include header in response
+	add_header X-Served-By $hostname;
+
 	# Add index
 	index index.html
 
 	server_name _;
+
+	location / {
+		# First attempt to serve a request as file, then
+		# as directiory, then fall back to dispalying a 404 ERROR.
+		try_files $uri $uri/ =404;
+	}
 
 	location /hbnb_static {
 		# Select directory
@@ -60,11 +82,18 @@ server {
 		autoindex off;
 	}
 
-	location / {
-		# First attempt to serve a request as file, then
-		# as directiory, then fall back to dispalying a 404 ERROR.
-		try_files $uri $uri/ =404;
+	location /redirect_me {
+		# Have fun with redirection
+		return 301 https://www/youtube.com/watch?v=QH2-TGUlw4;
 	}
+
+	# Create a custome 404 error page
+	error_page 404 /404.html;
+	location = /404.html {
+		# to ensure it cannot be accessed directly by clients
+		internal;
+	}
+
 }' > "$nginx_conf"
 
 
