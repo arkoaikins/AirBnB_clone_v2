b35e012d7034fce5d4bff8f35e233503d5453cb7
arkoaikins <arkoaikins@gmail.com>
2023-10-12 17:32:17 +0000

    add task 1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,10 +4,34 @@ Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 
-from fabric.api import put, run, env
+from fabric.api import put, run, env, local
 import os
+from datetime import datetime
 
 env.hosts = ['54.90.14.86', '54.160.114.180']
+
+
+def do_pack():
+    """
+    generates a .tgz archive from the contents of web_static
+    Returns:The archive path if created correctly else return None
+    """
+    try:
+        local("mkdir -p versions")
+        # create the time for the archive files
+        arc_time = datetime.now().strftime('%Y%m%d%H%M%S')
+
+        # create the compressed file
+        arc_path = "versions/web_static_{}.tgz".format(arc_time)
+        local("tar -czvf {} web_static".format(arc_path))
+        # check if the archive was correctly generated
+        if local("test -e {}".format(arc_path), capture=True):
+
+            return arc_path
+        else:
+            return None
+    except Exception as e:
+        pass
 
 
 def do_deploy(archive_path):
