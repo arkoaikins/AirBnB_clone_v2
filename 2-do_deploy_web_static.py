ffd0cd66a0c5b951c87d6ec6fd959d1f1b09cf58
arkoaikins <arkoaikins@gmail.com>
2023-10-12 17:09:21 +0000

    de deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to webservers
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
+
+from fabric.api import put, run, env
 import os
-from fabric.api import env, run, put, cd
 
 env.hosts = ['54.90.14.86', '54.160.114.180']
 
@@ -16,37 +18,30 @@ def do_deploy(archive_path):
     Returns:
         True if all operations have been done correctly, else return False
     """
-
     # Check if the archive file exists locally
     if not os.path.exists(archive_path):
         return False
-
-    # Get the filename from the archive_path
-    arc_file = os.path.basename(archive_path)
-
-    # Upload the archive to the /tmp/ directory of the web server
-    put(archive_path, "/tmp/{}".format(arc_file))
-
-    # Create a directory with the archive filename without the extension
-    arc_dir = arc_file[:-4]
-
-    # Uncompress the archive to a folder on the web server
-    with cd('/data/web_static/releases'):
-        run("mkdir -p {}".format(arc_dir))
-        with cd('/tmp'):
-            run("tar -xzf {} -C /data/web_static/releases/{}/"
-                .format(arc_file, arc_dir))
-
-    # Delete the archive from the web server
-    run("rm /tmp/{}".format(arc_file))
-
-    # Delete the symbolic link /data/web_static/current from the web server
-    with cd('/data/web_static'):
-        run("rm current", warn_only=True)
-
-    # Create a new symbolic link /data/web_static/current
-    with cd('/data/web_static/releases'):
-        run("ln -s {} /data/web_static/current".format(arc_dir))
-
-    # Return True if all operations have been done correctly
-    return True
+    try:
+        # Get the filename from the archive_path
+        arc_file = os.path.basename(archive_path)
+        # Create a directory with the archive filename without the extension
+        arc_dir = arc_file.split(".")[0]
+        # Folder on the webserver
+        serv_fld = "/data/web_static/releases/"
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, '/tmp/')
+        # Uncompress the archive to a folder on the web server
+        run('mkdir -p {}{}/'.format(serv_fld, arc_dir))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        # Delete the archive from the web server
+        run('rm /tmp/{}'.format(arc_file))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(serv_fld, arc_dir))
+        # Delete the symbolic link /data/web_static/current from the web server
+        run('rm -rf {}{}/web_static'.format(serv_fld, arc_dir))
+        run('rm -rf /data/web_static/current')
+        # Create a new symbolic link /data/web_static/current
+        run('ln -s {}{}/ /data/web_static/current'.format(serv_fld, arc_dir))
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
