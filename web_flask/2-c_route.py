350c3bc0d36d62c4dfdd3ebeed67c14e5d3e637b
arkoaikins <arkoaikins@gmail.com>
2023-10-20 23:03:44 +0000

    write flask app that display value of text

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+Route "/" displays "Hello HBNB!"
+Route "/hbnb" displays "HBNB"
+Route "/c/<text>:displays "C" followed by the value of the text variable
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def web_app():
+    """
+    the home route which displays Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def web_hbnb():
+    """
+    hbnb route to return HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def web_text(text):
+    return "C " + text.replace("_", " ")
+
+
+"""listening on 0.0.0.0 and port 5000"""
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
